607629a8efbac7265e1e99da9bbd2108aa445390
Diosa34 <superalex.osa@yandex.ru>
2022-03-21 01:17:24 +0300

    Gradle has been added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     java
+    `maven-publish`
+}
+
+publishing {
+    publications{
+        register<MavenPublication>("Beautiful"){
+            from(components["java"])
+        }
+    }
 }
